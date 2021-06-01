f6de1134cf93eca1bff442d2ff210fccfa56263e
Alina Govoruhina <delirioussoymilk@gmail.com>
2021-06-01 18:02:51 +0300

    add no normalization app

--- App/App.py
+++ App/App.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from predict_page import show_predict_page
+from explore_page_NN import show_explore_page_NN
 from explore_page_LM import show_explore_page_LM
 from explore_page_PS import show_explore_page_PS
 from explore_page_subreddits import show_explore_page_subreddits
 
-page = st.sidebar.selectbox("Choose page", ("Predict", "Explore Porter Stemmer model", "Explore WordNetLemmatizer model", "Show Subreddit Data (Lemmatizer)"))
+page = st.sidebar.selectbox("Choose page", ("Predict", "Explore model Without Normalization", "Explore Porter Stemmer model", "Explore WordNetLemmatizer model", "Show Subreddit Data (Lemmatizer)"))
 
 if page == "Predict":
     show_predict_page()
+if page == "Explore model Without Normalization":
+    show_explore_page_NN()
 if page == "Explore Porter Stemmer model":
     show_explore_page_PS()
 if page == "Explore WordNetLemmatizer model":
--- App/predict_page.py
+++ App/predict_page.py
@@ -6,6 +6,82 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
 # 
+# NO NORMALIZATION
+# 
+
+def cleanPost(post):
+    urls = re.compile(r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})')
+    numbers = re.compile(r'\d+(\.\d+)?')
+    posessivePronouns = re.compile(r"’s")
+    apostrophe=re.compile(r"’")
+    someSigns =re.compile(r"\\n|\\r")
+    punctuation = re.compile(r"[^\w\s]")
+    whitespaces = re.compile(r'\s+')
+    leadTrailWhitespace = re.compile(r'^\s+|\s+?$')
+    
+    cleanPost = post.lower()
+    cleanPost = urls.sub('url',cleanPost)
+    cleanPost = numbers.sub('nmbr',cleanPost)
+    cleanPost = posessivePronouns.sub('',cleanPost)
+    cleanPost = apostrophe.sub('',cleanPost)
+    cleanPost = someSigns.sub('',cleanPost)
+    cleanPost = punctuation.sub(' ',cleanPost)
+    cleanPost = whitespaces.sub(' ',cleanPost)
+    cleanPost = leadTrailWhitespace.sub('',cleanPost)
+    
+    # stop words
+    with open('./../stop_words_no_punct.data', 'rb') as filehandle:
+        # read the data as binary data stream
+        stop_words_no_punct = pickle.load(filehandle)
+        
+    post_without_stop_words = []
+
+    text_tokens = word_tokenize(cleanPost)
+    tokens_without_stop_words = [word for word in text_tokens if not word in stop_words_no_punct]
+    post_without_stop_words = (" ").join(tokens_without_stop_words)
+
+    cleanPost = post_without_stop_words
+    
+    return cleanPost
+
+def find_features_NN(post):
+    words = word_tokenize(post)
+    features = {}
+    
+    #read generated features for Lemmatizer
+    with open('./../NoNormalization/word_features.data', 'rb') as filehandle:
+        # read the data as binary data stream
+        word_features = pickle.load(filehandle)
+    
+    for word in word_features:
+        features[word] = (word in words)
+    return features
+
+def classifySubreddit_NN(post):
+    modelFile = './../NoNormalization/Models/finalized_model_subreddits.sav'
+    post = cleanPost(post)
+    
+    loaded_model = pickle.load(open(modelFile, 'rb'))
+    
+    featureset = find_features_NN(post)
+    
+    result = loaded_model.classify(featureset)
+        
+    return transformSubreddit(result)
+
+def classifyDepression_NN(post):
+    modelFile = './../NoNormalization/Models/finalized_model_is_depression.sav'
+    post = cleanPost(post)
+    
+    loaded_model = pickle.load(open(modelFile, 'rb'))
+    
+    featureset = find_features_NN(post)
+    
+    result = loaded_model.classify(featureset)
+        
+    return transformDepression(result)
+
+# 
 # PORTER STEMMER
 # 
 
@@ -216,6 +292,15 @@ def show_predict_page():
 
     classify = st.button("Classify")
     if classify:
+        classifyResult_NN = classifySubreddit_NN(post)
+        depressionResult_NN = classifyDepression_NN(post)
+
+        st.header('Using No Normalization:')
+        st.subheader('Subreddit classifier:')
+        st.write('Post is likely from '+ classifyResult_NN)
+        st.subheader('Is r/depression post:')
+        st.write('Post is likely '+ depressionResult_NN)
+
         classifyResult_PS = classifySubreddit_PS(post)
         depressionResult_PS = classifyDepression_PS(post)
 
